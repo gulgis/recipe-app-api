068317e256eae3e21572aac954c4bae686bd1d26
Willian Gulgielmin <g.will@sempreceub.com>
2020-07-20 16:22:55 -0300

    Correcting problem with init.py file in core tests folder

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                            PermissionsMixin
+    PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -12,5 +13,12 @@ class ModelTests(TestCase):
             password=password
         )
 
-        self.asserEqual(user.email,email)
+        self.asserEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email for a new user is normalized"""
+        email = 'test@BRASIL.COM'
+        user = get_user_model().objetcs.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
