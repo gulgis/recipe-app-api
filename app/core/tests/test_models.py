82099865080142843301c8421e6550583bff1048
Willian Gulgielmin <g.will@sempreceub.com>
2020-07-20 18:45:29 -0300

    Creating test for invalid email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,17 +8,23 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'mail@mail.com'
         password = 'Testpass123'
-        user = get_user_model().objetcs.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
 
-        self.asserEqual(user.email, email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test email for a new user is normalized"""
         email = 'test@BRASIL.COM'
-        user = get_user_model().objetcs.create_user(email, 'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
